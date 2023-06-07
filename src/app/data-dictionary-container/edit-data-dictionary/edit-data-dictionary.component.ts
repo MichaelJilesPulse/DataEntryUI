31b4fcec57b5a205fa58ef99c766da291bbf0f11
Michael Jiles <MJiles@AcrossHealthcare.com>
2023-06-07 17:23:44 -0400

    DataDictionary crud functional

--- src/app/data-dictionary-container/edit-data-dictionary/edit-data-dictionary.component.ts
+++ src/app/data-dictionary-container/edit-data-dictionary/edit-data-dictionary.component.ts
@@ -1,57 +1,82 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {DataDictionary} from '../../models/data-dictionary/data-dictionary';
 import {FormBuilder, FormControl, Validators} from '@angular/forms';
 import {DataDictionaryVariableRef} from '../../models/data-dictionary/data-dictionary-variable-ref';
+import {DataDictionaryService} from '../../services/data-dictionary-service';
+import {DdVariable} from '../../models/dd-variable/dd-variable';
+import {AlertDialogComponent} from '../../utilities/alert-dialog/alert-dialog.component';
+import {MatDialog} from '@angular/material/dialog';
+import {DdVariableRef} from '../../models/dd-variable/dd-variable-ref';
+import {Constants} from '../../constants/constants';
+import {CrudActions} from '../../enums/crud-actions';
 
 @Component({
   selector: 'app-edit-data-dictionary',
   templateUrl: './edit-data-dictionary.component.html',
   styleUrls: ['./edit-data-dictionary.component.scss']
 })
-export class EditDataDictionaryComponent implements OnInit {
+export class EditDataDictionaryComponent implements OnInit, OnDestroy{
 
   @Output()
-  onSave = new EventEmitter<DataDictionary>();
+  onSave = new EventEmitter<{dictionary: DataDictionary, action: CrudActions}>();
   @Input()
   dictionary: DataDictionary;
 
-  id: FormControl = new FormControl('', Validators.required);
+  actions = CrudActions;
+
+  id: FormControl = new FormControl('');
   name: FormControl = new FormControl('', Validators.required);
-  variableRefs = new FormControl<DataDictionaryVariableRef[]>([], Validators.required);
+  regName: FormControl = new FormControl('');
+  notes: FormControl = new FormControl('');
+  assignedTo: FormControl = new FormControl('');
+  originalId: FormControl = new FormControl('');
+  copiedFrom: FormControl = new FormControl('');
+  published: FormControl = new FormControl('');
+
+  variableRefs = new FormControl<DdVariableRef[]>([]);
 
   form = this.fb.group({
     id: this.id,
     name: this.name,
+    regName: this.regName,
+    notes: this.notes,
+    assignedTo: this.assignedTo,
+    originalId: this.originalId,
+    copiedFrom: this.copiedFrom,
+    published: this.published,
     variableRefs: this.variableRefs
   });
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              public dictionaryService: DataDictionaryService,
+              public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    if (this.dictionary !== undefined) {
-      this.id.setValue(this.dictionary.id);
-      this.name.setValue(this.name);
+    this.id.disable();
+    this.originalId.disable();
+    this.copiedFrom.disable();
+    this.published.disable();
+    if (this.dictionary.id !== '') {
+      this.dictionaryService.getDataDictionary({id: this.dictionary.id}).subscribe(dict => {
+        this.dictionary = dict;
+        console.log(this.dictionary);
+        this.id.setValue(this.dictionary.id);
+        this.name.setValue(this.dictionary.name);
+        this.regName.setValue(this.dictionary.regname);
+        this.notes.setValue(this.dictionary.notes);
+        this.assignedTo.setValue(this.dictionary.assignedTo);
+        this.originalId.setValue(this.dictionary.originalId);
+        this.copiedFrom.setValue(this.dictionary.copiedFromId);
+        this.published.setValue(this.dictionary.published);
+        this.variableRefs.setValue(this.dictionary.variables);
+      })
     } else {
       this.dictionary = new DataDictionary();
-      this.dictionary.id = 'dictID';
-      this.dictionary.name = 'dictname';
-      this.dictionary.variableRefs.push({
-        dataDictionaryId: this.dictionary.id,
-        id: 'refOneId',
-        sortPos: 1,
-        ddVariableId: 'ddVarOneId'
-      });
-      this.dictionary.variableRefs.push({
-        dataDictionaryId: this.dictionary.id,
-        id: 'refTwoId',
-        sortPos: 2,
-        ddVariableId: 'ddVarTwoId'
-      });
     }
   }
 
-  save() {
-    this.onSave.emit();
+  emit(action: CrudActions) {
+    this.onSave.emit({dictionary: this.buildDictionary(), action});
   }
 
   refsChanged(refs: any) {
@@ -59,4 +84,37 @@ export class EditDataDictionaryComponent implements OnInit {
     this.variableRefs.updateValueAndValidity();
   }
 
+  onCancel() {
+    this.onSave.emit(undefined);
+  }
+
+  buildDictionary(): DataDictionary {
+    return {
+      id: this.dictionary.id,
+      originalId: this.originalId.value,
+      assignedTo: this.assignedTo.value,
+      copiedFromId: this.copiedFrom.value,
+      name: this.name.value,
+      notes: this.notes.value,
+      published: new Date(),
+      regname: this.regName.value,
+      variables: this.variableRefs.value!,
+      created: this.dictionary.created,
+      modified: this.dictionary.modified,
+      createdBy: this.dictionary.createdBy,
+      modifiedBy: Constants.userId
+    };
+  }
+
+  ngOnDestroy() {
+    this.id.setValue('');
+    this.name.setValue('');
+    this.regName.setValue('');
+    this.notes.setValue('');
+    this.assignedTo.setValue('');
+    this.originalId.setValue('');
+    this.copiedFrom.setValue('');
+    this.published.setValue('');
+    this.variableRefs.setValue([]);
+  }
 }
--- src/app/services/data-dictionary-service.ts
+++ src/app/services/data-dictionary-service.ts
@@ -1,5 +1,8 @@
 import {Injectable} from '@angular/core';
 import {ApiClient} from './api-client';
+import {IdRequest} from '../models/requests/id-request';
+import {Observable, take} from 'rxjs';
+import {DataDictionary} from '../models/data-dictionary/data-dictionary';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +15,29 @@ import {ApiClient} from './api-client';
 
   getDataDictionaries() {
     return this.apiClient.post(this.servicePath + 'get-all', null);
+  }
+
+  getDataDictionary(req: IdRequest) : Observable<DataDictionary> {
+    return this.apiClient.post(this.servicePath + 'get', req).pipe(take(1));
+  }
+
+  createDataDictionary(req: DataDictionary) : Observable<DataDictionary> {
+    return this.apiClient.post(this.servicePath + 'create', req).pipe(take(1));
+  }
+
+  cloneDataDictionary(req: DataDictionary) : Observable<DataDictionary> {
+    return this.apiClient.post(this.servicePath + 'clone', req).pipe(take(1));
+  }
+
+  newVersionDataDictionary(req: DataDictionary) : Observable<DataDictionary> {
+    return this.apiClient.post(this.servicePath + 'create-new-version', req).pipe(take(1));
+  }
+
+  updateDataDictionary(req: DataDictionary) : Observable<DataDictionary> {
+    return this.apiClient.post(this.servicePath + 'update', req).pipe(take(1));
+  }
+
+  publishDataDictionary(req: DataDictionary) : Observable<DataDictionary> {
+    return this.apiClient.post(this.servicePath + 'publish', req).pipe(take(1));
   }
 }
